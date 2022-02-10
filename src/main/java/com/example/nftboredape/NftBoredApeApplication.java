37458204dca5e91a1b6c5a49b5dff453f0eb322e
Tyler Fitzgerald <tylerjf1993@gmail.com>
2022-02-10 12:28:57 -0500

    initial commit

---
+++ src/main/java/com/example/nftboredape/NftBoredApeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nftboredape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftBoredApeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftBoredApeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nftboredape/NftBoredApeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nftboredape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftBoredApeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
